1ea2426d26eef8e56baf117998e35a97ced87339
choeyunseo <choeyunseo4060@naver.com>
2023-12-01 23:35:33 +0900

    modify queue.c

--- queue/queue.c
+++ queue/queue.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} node;
+
+typedef struct Queue {
+    struct Queue* front;
+    struct Queue* rear;
+} queue;
 
